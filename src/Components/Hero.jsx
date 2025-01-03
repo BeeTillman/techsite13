abcb5fc40d47275e8aa85561a168d027bb8df25a
Bee Tillman <bpt3creations@gmail.com>
2025-01-03 15:50:24 +0000

    Carousel Delay + New Slide Images

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,8 +3,8 @@ import { Carousel } from "react-responsive-carousel";
 import "../styles.css";
 
 import image1 from "../images/tpsiteOfficeStock1.jpg";
-import image2 from "../images/tpsiteOfficeStock1.jpg";
-import image3 from "../images/tpsiteOfficeStock1.jpg";
+import image2 from "../images/tpsiteSkylineStock6.jpg";
+import image3 from "../images/tpsiteSkylineStock8.jpg";
 
 const quotes = [
   "Dedicated exclusively to the ServiceNow Platform.",
@@ -35,17 +35,17 @@ const Hero = () => {
   return (
     <section ref={sectionRef} id="hero" className="hero-section section-transition">
       <div className="section-overlay overlay-bottom"></div>
-      <Carousel autoPlay infiniteLoop showThumbs={false} showStatus={false}>
+      <Carousel autoPlay infiniteLoop showThumbs={false} showStatus={false} interval={10000}>
         <div>
-          <img src={image1} alt="Empowering your business" />
+          <img src={image1} alt={quotes[0]} />
           <p className="legend">{quotes[0]}</p>
         </div>
         <div>
-          <img src={image2} alt="Transforming technology" />
+          <img src={image2} alt={quotes[1]} />
           <p className="legend">{quotes[1]}</p>
         </div>
         <div>
-          <img src={image3} alt="Trusted partner" />
+          <img src={image3} alt={quotes[2]} />
           <p className="legend">{quotes[2]}</p>
         </div>
       </Carousel>
